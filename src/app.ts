819052a75848e73d4018cd3917bbf73cd566cfe7
DimVasDev <d.v.vassilev@gmail.com>
2025-08-12 22:11:58 +0300

    feat/ init slot class

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import Slot from './controller/slot.js';
 
 const app = express();
 
+const slot = new Slot();
+
 app.listen(() => {
-  console.log(`Server running on port ${3000}`);
+    console.log(`Server running on port ${3000}`);
 });
